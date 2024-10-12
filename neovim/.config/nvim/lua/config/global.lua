4f1e3e05d01790ff1e11b54d037b2af8811e6bc7
Matteo Vacalebri <thet3o@Matteos-MacBook-Air.local>
2024-10-12 09:30:28 +0200

    Added multicursor plugin

--- neovim/.config/nvim/lua/config/global.lua
+++ neovim/.config/nvim/lua/config/global.lua
@@ -2,13 +2,17 @@ vim.o.autoread = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
 -- Diagnostic Configuration
 vim.diagnostic.config({
-  virtual_text = true,
-  signs = true,
-  underline = true,
-  update_in_insert = true,
-  severity_sort = false,
+    virtual_text = true,
+    signs = true,
+    underline = true,
+    update_in_insert = true,
+    severity_sort = false,
 })
 
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
