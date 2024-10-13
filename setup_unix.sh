5f42da05026562da726db9fa243914b62394f1be
Matteo Vacalebri <thet3o@Matteos-MacBook-Air.local>
2024-10-13 08:34:50 +0200

    Added to setup_unix.sh base function to check presence of packages

--- setup_unix.sh
+++ setup_unix.sh
@@ -1,15 +1,33 @@
-if [[ $OSTYPE == "darwin"* ]]; then
-	if [[ $(command -v brew) == "" ]]; then
-	    echo "Installing Homebrew"
-	    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Only in this function package_exists true is 1 and false is 0
+package_exists(){
+    return command -v lala > /dev/null 2>&1 || echo 1
+}
+
+install_cmd(){
+    if [[ $OSTYPE == "darwin"* ]]; then
+        if package_exists "brew" ; then
+            brew update
+            return brew install $1
         else
-	    echo "Updating Homebrew"
-	    brew update
-	fi
-	echo "Installing stow"
-	brew install stow
-elif [[ $OSTYPE == "linux-gnu"* ]]; then
-	echo "It's Linux"
-else
-	echo "No it's not Apple"
-fi
+            echo "Installing Homebrew"
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            return brew
+        fi
+    elif [[ $OSTYPE == "linux-gnu"* ]]; then
+        return "linux"
+    fi
+}
+
+baseUnixDeps=("curl wget stow")
+base_deps_check(){
+    for PACKAGE in $baseUnixDeps
+    do
+        if $(package_exists $PACKAGE) ; then
+            install_cmd $PACKAGE
+        else
+            echo $PACKAGE installed
+        fi
+    done
+}
+
+base_deps_check
